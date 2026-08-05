abbf917e6769fb6d06aef77b087c70b741b19c02
Matthew Williams <matthew.j.williams1989+github@gmail.com>
2026-08-04 23:28:00 -0400

    update

--- app.py
+++ app.py
@@ -589,9 +589,9 @@ class GolfTrajectoryGenerator:
         
         path_pt[0] -= 40
         path_pt[1] -= 20
-        cv2.putText(im,"Club Path", path_pt,ft,1.4,(0,0,255),1)
+        cv2.putText(im,"Club Path", path_pt,ft,1.4,DKRED,1)
         path_pt[1] += 70
-        cv2.putText(im,"Club Face", path_pt,ft,1.4,(0,0,0),1)
+        cv2.putText(im,"Club Face", path_pt,ft,1.4,BLK,1)
         
         
         
